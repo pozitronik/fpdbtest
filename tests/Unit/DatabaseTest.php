d561cbea607a08599bfc6e9fffce9f942d48f98f
Pozitronik <antikillerxxl@gmail.com>
2024-04-15 15:36:01 +0400

    Unit test stub

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Tests\Unit;
+
+use Codeception\Test\Unit;
+use Tests\Support\UnitTester;
+
+class DatabaseTest extends Unit
+{
+
+    protected UnitTester $tester;
+
+    /**
+     * @Override
+     */
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
